80d09cd0980d815c4d83201ec83c3d930d84637d
rajeevsubbian <rajeev.subbian@gmail.com>
2024-07-23 20:55:30 +0100

    Simple change done for July 23, 2024

---
+++ src/LinkedListExample.java
@@ -0,0 +1,36 @@
+
+public class LinkedListExample {
+    Node head;
+
+    // Insert at the end
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    // Display the linked list
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkedListExample list = new LinkedListExample();
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.display();
+    }
+}
